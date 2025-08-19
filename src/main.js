51bdc8d4d5a5182e6c3640f4222c0767cbdf492e
Azat Ziganshin <azat-physick@ya.ru>
2025-08-19 17:17:42 +0300

    первый коммит

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@
  * @returns {number}
  */
 function calculateSimpleRevenue(purchase, _product) {
-   // @TODO: Расчет выручки от операции
+  const { discount, sale_price, quantity } = purchase;
+  const discountFactor = 1 - discount / 100;
+  revenue = sale_price * quantity * discountFactor;
+  return revenue;
 }
 
 /**
@@ -16,7 +19,17 @@ function calculateSimpleRevenue(purchase, _product) {
  * @returns {number}
  */
 function calculateBonusByProfit(index, total, seller) {
-    // @TODO: Расчет бонуса от позиции в рейтинге
+  const { profit } = seller;
+
+  if (index === 0) {
+    return 0.15 * profit;
+  } else if (index === 1 || index === 2) {
+    return 0.1 * profit;
+  } else if (index === total - 1) {
+    return 0;
+  } else {
+    return 0.05 * profit;
+  }
 }
 
 /**
@@ -26,19 +39,90 @@ function calculateBonusByProfit(index, total, seller) {
  * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
  */
 function analyzeSalesData(data, options) {
-    // @TODO: Проверка входных данных
+  // @TODO: Проверка входных данных
+  if (
+    !data ||
+    (!Array.isArray(data.sellers) &&
+      !Array.isArray(data.customers) &&
+      !Array.isArray(data.products) &&
+      !Array.isArray(data.purchase_records)) ||
+    (data.sellers.length === 0 &&
+      data.customers.length === 0 &&
+      data.products.length === 0 &&
+      data.purchase_records.length === 0)
+  ) {
+    throw new Error("Некорректные входные данные");
+  }
 
-    // @TODO: Проверка наличия опций
+  // @TODO: Проверка наличия опций
+  const { calculateRevenue, calculateBonus } = options;
+  if (
+    !(typeof calculateRevenue === "function") ||
+    !(typeof calculateBonus === "function")
+  ) {
+    throw new Error("Чего-то не хватает");
+  }
 
-    // @TODO: Подготовка промежуточных данных для сбора статистики
+  // @TODO: Подготовка промежуточных данных для сбора статистики
+  const sellerStats = data.sellers.map((seller) => ({
+    id: seller.id,
+    name: `${seller.first_name} ${seller.last_name}`,
+    revenue: 0,
+    profit: 0,
+    sales_count: 0,
+    products_sold: {},
+  }));
 
-    // @TODO: Индексация продавцов и товаров для быстрого доступа
+  // @TODO: Индексация продавцов и товаров для быстрого доступа
+  const sellerIndex = Object.fromEntries(
+    sellerStats.map((item) => [item.id, item])
+  );
+  const productIndex = Object.fromEntries(
+    data.products.map((item) => [item.sku, item])
+  );
 
-    // @TODO: Расчет выручки и прибыли для каждого продавца
+  // @TODO: Расчет выручки и прибыли для каждого продавца
+  data.purchase_records.forEach((record) => {
+    const seller = sellerIndex[record.seller_id];
+    seller.sales_count++;
+    seller.revenue += record.total_amount;
 
-    // @TODO: Сортировка продавцов по прибыли
+    // Расчёт прибыли для каждого товара
+    record.items.forEach((item) => {
+      const product = productIndex[item.sku];
+      cost = product.purchase_price * item.quantity;
+      revenue = calculateSimpleRevenue(item, product);
+      profit = revenue - cost;
+      seller.profit += profit;
 
-    // @TODO: Назначение премий на основе ранжирования
+      // Учёт количества проданных товаров
+      if (!seller.products_sold[item.sku]) {
+        seller.products_sold[item.sku] = 0;
+      }
+      seller.products_sold[item.sku] += item.quantity;
+    });
+  });
 
-    // @TODO: Подготовка итоговой коллекции с нужными полями
+  // @TODO: Сортировка продавцов по прибыли
+  sellerStats.sort((a, b) => b.profit - a.profit);
+
+  // @TODO: Назначение премий на основе ранжирования
+  sellerStats.forEach((seller, index) => {
+    seller.bonus = calculateBonusByProfit(index, sellerStats.length, seller);
+    seller.top_products = Object.entries(seller.products_sold)
+      .map(([sku, quantity]) => ({ sku, quantity }))
+      .sort((a, b) => b.quantity - a.quantity)
+      .slice(0, 10);
+  });
+
+  // @TODO: Подготовка итоговой коллекции с нужными полями
+  return sellerStats.map((seller) => ({
+    seller_id: seller.id,
+    name: seller.name,
+    revenue: +seller.revenue.toFixed(2),
+    profit: +seller.profit.toFixed(2),
+    sales_count: seller.sales_count,
+    top_products: seller.top_products,
+    bonus: +seller.bonus.toFixed(2),
+  }));
 }
