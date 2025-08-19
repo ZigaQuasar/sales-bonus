f1e634d65c5533a655136b972b72be3325c8c3b3
Azat Ziganshin <azat-physick@ya.ru>
2025-08-19 18:01:00 +0300

    второй коммит

--- src/main.js
+++ src/main.js
@@ -42,13 +42,13 @@ function analyzeSalesData(data, options) {
   // @TODO: Проверка входных данных
   if (
     !data ||
-    (!Array.isArray(data.sellers) &&
-      !Array.isArray(data.customers) &&
-      !Array.isArray(data.products) &&
+    (!Array.isArray(data.sellers) ||
+      !Array.isArray(data.customers) ||
+      !Array.isArray(data.products) ||
       !Array.isArray(data.purchase_records)) ||
-    (data.sellers.length === 0 &&
-      data.customers.length === 0 &&
-      data.products.length === 0 &&
+    (data.sellers.length === 0 ||
+      data.customers.length === 0 ||
+      data.products.length === 0 ||
       data.purchase_records.length === 0)
   ) {
     throw new Error("Некорректные входные данные");
